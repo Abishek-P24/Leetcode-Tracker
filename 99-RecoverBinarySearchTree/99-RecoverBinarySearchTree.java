3c033d7a24bbccc2dc6934fc490ebf4391fb2f9a
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:47:05 +0530

    Create 99-RecoverBinarySearchTree

---
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -0,0 +1,35 @@
+// Last updated: 7/10/2026, 9:47:12 AM
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        // Swap the values of the two incorrect nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+
+        // Detect violation
+        if (prev.val > root.val) {
+
+            if (first == null) {
+                first = prev;
+            }
+
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+}
