a60c0200e86c4117131b1260a1788781036d614d
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:43:26 +0530

    Create 1231-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1231-ReplaceElementsWithGreatestElementOnRightSide/1231-ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,12 @@
+// Last updated: 7/10/2026, 9:43:33 AM
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        for (int i=0;i<arr.length;i++){
+            int max=-1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>max) max=arr[j];
+            } arr[i]=max;
+        }
+        return arr;
+    }
+}
