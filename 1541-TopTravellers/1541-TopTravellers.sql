a9bce927be19c2ed1d5bab8e3b4cc182de6ed91b
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:43:07 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/10/2026, 9:43:13 AM
+SELECT
+    u.name,
+    COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
