d467e29ba844e8283399d69943eb45ed61b4b104
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:43:18 +0530

    Create 1352-MaximumProfitInJobScheduling

---
+++ 1352-MaximumProfitInJobScheduling/1352-MaximumProfitInJobScheduling.java
@@ -0,0 +1,27 @@
+// Last updated: 7/10/2026, 9:43:24 AM
+import java.util.*;
+
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+
+        for (int[] job : jobs) {
+            int cur = dp.floorEntry(job[0]).getValue() + job[2];
+            dp.put(job[1], Math.max(dp.lastEntry().getValue(), cur));
+        }
+
+        return dp.lastEntry().getValue();
+    }
+}
