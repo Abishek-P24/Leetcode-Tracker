8915058b2082be5f7c2bb0af0f03af4cfacb6934
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:43:00 +0530

    Create 1829-MaximumUnitsOnATruck

---
+++ 1829-MaximumUnitsOnATruck/1829-MaximumUnitsOnATruck.java
@@ -0,0 +1,14 @@
+// Last updated: 7/10/2026, 9:43:07 AM
+class Solution {
+    public int maximumUnits(int[][] B, int T) {
+        Arrays.sort(B, (a,b) -> b[1] - a[1]);
+        int ans = 0;
+        for (int[] b : B) {
+            int count = Math.min(b[0], T);
+            ans += count * b[1];
+            T -= count;
+			if (T == 0) return ans;
+        }
+        return ans;
+    }
+}
