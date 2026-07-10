44dad70a445a191a747be6748a421d493add06e1
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:42:11 +0530

    Create 3371-HarshadNumber

---
+++ 3371-HarshadNumber/3371-HarshadNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/10/2026, 9:42:17 AM
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int a=x;
+        while(a!=0){
+            sum+=a%10;
+            a/=10;
+        }
+        if(x%sum==0){
+        return sum;
+        }
+        else{
+            return-1;
+        }
+    }
+}
