60795b4caa19c8f4ad772a6c470e7481f6facda8
Abishek P <abishekprabaharan3@gmail.com>
2026-07-10 09:48:05 +0530

    Create 64-MinimumPathSum

---
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -0,0 +1,21 @@
+// Last updated: 7/10/2026, 9:48:12 AM
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n=grid.length,m=grid[0].length;
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0){
+                    continue;
+                }else if(i==0){
+                    grid[i][j]+=grid[i][j-1];
+                }else if(j==0){
+                    grid[i][j]+=grid[i-1][j];
+                }else{
+                    grid[i][j]+=Math.min(grid[i][j-1],grid[i-1][j]);
+                }
+            }
+        }
+        return grid[n-1][m-1];
+    }
+}
